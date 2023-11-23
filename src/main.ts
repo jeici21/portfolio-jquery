6fe305932dc4bc321027c4fbaf7f4c52c80456db
Jorge Castro <david92_jc@yahoo.es>
2023-11-23 17:47:08 -0500

    paquetes actualizados

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import $ from 'jquery'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './style.css'
 import { App, setupApp } from './App'
