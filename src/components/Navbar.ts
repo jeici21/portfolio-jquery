94b02e018e6db5962bed94177704fc1fddd4a524
Jorge Castro <david92_jc@yahoo.es>
2023-11-25 17:13:34 -0500

    Navbar corregido

--- src/components/Navbar.ts
+++ src/components/Navbar.ts
@@ -1,45 +1,34 @@
-import { css } from "@emotion/css"
 import $ from 'jquery'
 
-const span = css`
-  &:hover {
-    color: silver;
-  }
-`
-
 export const setupNavbar = () => {
-  const links = [
-    { text: "Sobre mí", href: "#sobre-mi" },
-    { text: "Formación y experiencia", href: "#formacion-y-experiencia" },
-    { text: "Aptitudes", href: "#aptitudes" },
-    { text: "Proyectos", href: "#proyectos" },
-  ];
-
-  $.each(links, (index, link) => {
-    $('#links').append(/*html*/`
-      <li class="nav-item ps-5">
-        <a class="nav-link link-light ${index === 0 && 'fw-bold'}" href=${link.href}>
-          <span class=${span}>${link.text}</span>
-        </a>
-      </li>
-    `).find('a').on('click', function () {
-      $('#links li a').removeClass('fw-bold');
-      $(this).addClass('fw-bold');
-    });
-  })
+  $('#links li a').on('click', function () {
+    $('#links li a').removeClass('active');
+    $(this).addClass('active');
+  });
 }
 
 export const Navbar = /*html*/`
-  <nav class="navbar navbar-expand-sm bg-primary">
-    <div class="container-fluid">
-      <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse"
+  <nav class="navbar navbar-expand-sm bg-primary sticky-top" data-bs-theme="dark">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
         data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
         aria-expanded="false" aria-label="Toggle navigation">
         <i class="bi bi-list text-light"></i>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0 fst-italic" id="links"></ul>
+        <ul class="navbar-nav me-auto fst-italic" id="links">
+          <li class="nav-item ps-5">
+            <a class="nav-link active" href="#sobre-mi">Sobre mí</a>
+          </li>
+          <li class="nav-item ps-5">
+            <a class="nav-link" href="#formacion-y-experiencia">Formación y experiencia</a>
+          </li>
+          <li class="nav-item ps-5">
+            <a class="nav-link" href="#aptitudes">Aptitudes</a>
+          </li>
+          <li class="nav-item ps-5">
+            <a class="nav-link" href="#proyectos">Proyectos</a>
+          </li>
+        </ul>
       </div>
-    </div>
   </nav>
 `
